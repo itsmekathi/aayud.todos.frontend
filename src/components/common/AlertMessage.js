b8aa330d003b3ef0062798edd1bd53ef4f3fad2c
Kathiravan B <itsmekathi92@gmail.com>
2024-11-24 21:29:26 +0530

    Added validation to todo form

---
+++ src/components/common/AlertMessage.js
@@ -0,0 +1,13 @@
+import { Alert } from "react-bootstrap";
+
+const AlertMessage = (props) => {
+  return (
+    <>
+      <Alert variant={props.variant} key={props.variant}>
+        <i className="bi bi-exclamation-diamond-fill">{props.message}</i>
+      </Alert>
+    </>
+  );
+};
+
+export default AlertMessage;
--- src/components/todo/AddTodoForm/AddTodoForm.js
+++ src/components/todo/AddTodoForm/AddTodoForm.js
@@ -1,39 +1,56 @@
-import React, { useState } from 'react';
-import { Form, Button } from 'react-bootstrap';
-import './AddTodoForm.css';
+import { Form, Button } from "react-bootstrap";
+import "./AddTodoForm.css";
+import { useForm } from "react-hook-form";
 
 const AddTodoForm = ({ onAddTodo }) => {
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    await onAddTodo({ title, description });
+  const onSubmit = async (data) => {
+    console.log(`Form Submitted: ${data}`);
+    await onAddTodo(data);
   };
 
   return (
     <div className="add-todo-form">
-      <h1>Add todo</h1>
-      <Form onSubmit={handleSubmit}>
-        <Form.Group className="mb-3">
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <h1>Add todo</h1>
+        <Form.Group controlId="title" className="mb-3">
           <Form.Label>Title</Form.Label>
           <Form.Control
             type="text"
             placeholder="Enter todo title"
-            value={title}
-            onChange={e => setTitle(e.target.value)}
+            isInvalid={!!errors.title}
+            {...register("title", {
+              required: "Title is required.",
+              minLength: {
+                value: 5,
+                message: "Title must be at least 3 characters",
+              },
+            })}
           />
+          <Form.Control.Feedback type="invalid">
+            {errors.title?.message}
+          </Form.Control.Feedback>
         </Form.Group>
-        <Form.Group className="mb-3">
+        <Form.Group controlId="description" className="mb-3">
           <Form.Label>Description</Form.Label>
           <Form.Control
             as="textarea"
             rows={3}
-            style={{ resize: 'none' }}
-            placeholder="Enter todo description"
-            value={description}
-            onChange={e => setDescription(e.target.value)}
+            style={{ resize: "none" }}
+            placeholder="Enter description"
+            isInvalid={!!errors.description}
+            {...register("description", {
+              required: "Description is required.",
+            })}
           />
+          <Form.Control.Feedback type="invalid">
+            {errors.description?.message}
+          </Form.Control.Feedback>
         </Form.Group>
         <Button variant="primary" type="submit">
           Add Todo
